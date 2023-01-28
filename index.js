642f69cb29faf8c23e05d0637b743e81029d4bcc
Nipun VV <nipunvv@gmail.com>
2023-01-29 01:33:32 +0530

    Remove index.html

--- index.js
+++ index.js
@@ -5,12 +5,8 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-// app.get("/", (req, res) => {
-//   res.sendFile(__dirname + "/index.html");
-// });
-
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  console.log("user connected");
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
