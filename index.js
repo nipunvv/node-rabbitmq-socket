ca2d2a39c24d49887b13a1f3381744cd7d33ad4d
Nipun VV <nipunvv@gmail.com>
2023-01-29 03:18:50 +0530

    Fix CORS error on connecting socket

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 const http = require("http");
+
 const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 io.on("connection", (socket) => {
   console.log("user connected");
@@ -11,11 +16,11 @@ io.on("connection", (socket) => {
     console.log("user disconnected");
   });
 
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
+  socket.on("chat_message", (msg) => {
+    console.log("message: " + msg);
   });
 });
 
-server.listen(3000, () => {
+server.listen(3010, () => {
   console.log("listening on *:3010");
 });
