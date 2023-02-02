222ef9ba5cdae6af81bb1b9a9cc21475b9110551
Nipun VV <nipunvv@gmail.com>
2023-02-03 03:48:03 +0530

    Use mongodb as database

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
 
 const app = express();
 
@@ -13,8 +16,23 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
+const url = `mongodb://root:123456@mongodb:27017/messaging_db?authSource=admin`
+
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "App running" });
 });
 
 const PORT = process.env.PORT || 8080;
